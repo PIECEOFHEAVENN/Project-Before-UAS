d9a4f2c2fa5e552bfd23c07a2f946353cdbb2059
rizalNCC <rizal@bawana.com>
2026-06-25 17:21:11 +0700

    fix: trust Vercel proxy headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         // Register custom middleware aliases
         $middleware->alias([
             'auth.custom' => AuthMiddleware::class,
